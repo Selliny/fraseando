10abcb2e211378cdc7b40801199677f675adb902
Selliny <144755530+Selliny@users.noreply.github.com>
2024-06-08 00:24:07 -0300

    Create emotions.dart

---
+++ lib/pages/emotions.dart
@@ -0,0 +1,8 @@
+class Emotion {
+  final DateTime date;
+  final String type;
+  final int intensity;
+
+  Emotion({required this.date, required this.type, required this.intensity});
+}
+// modelos de dados(models)
